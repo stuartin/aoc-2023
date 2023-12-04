15fe6a79552e7500a90ca270e55b62af6d506913
stuartin <13344713+stuartin@users.noreply.github.com>
2023-12-03 20:13:06 -0800

    day03

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -6,17 +6,15 @@ function isHitIdx(i: number) {
 }
 
 type Symbol = {
-  id: string; // {lineIdx}-{symbolIdx}
   lineIdx: number;
   char: string;
   charIdx: number;
-  validIdx: [number, number, number];
   grid: string[];
-  numbersInGrid: SurroundingNumber[];
+  gridNumbers: GridNumber[];
 };
 
-type SurroundingNumber = {
-  chars: string;
+type GridNumber = {
+  char: string;
   idxs: number[];
 };
 
@@ -29,12 +27,10 @@ const parseInput = (rawInput: string) => {
     const symbolsOnLine = line.split("").reduce((acc, char, charIdx) => {
       if (char.match(SYMBOL_REGEX)) {
         acc.push({
-          id: `${lineIdx}-${charIdx}`,
           lineIdx,
           char,
           charIdx,
-          validIdx: [charIdx - 1, charIdx, charIdx + 1],
-          numbersInGrid: [],
+          gridNumbers: [],
           grid: [],
         });
       }
@@ -44,39 +40,35 @@ const parseInput = (rawInput: string) => {
     return [...arr, ...symbolsOnLine];
   }, [] as Symbol[]);
 
-  const symbolsWithGrid = symbols.map(
-    ({ id, lineIdx, char, charIdx, validIdx }) => {
-      const prevLineIdx = lineIdx - 1;
-      const nextLineIdx = lineIdx + 1;
+  const symbolsWithGrid = symbols.map(({ lineIdx, char, charIdx }) => {
+    const prevLineIdx = lineIdx - 1;
+    const nextLineIdx = lineIdx + 1;
 
-      const topRow = lines[prevLineIdx].slice(charIdx - 3, charIdx + 4);
-      const midRow = lines[lineIdx].slice(charIdx - 3, charIdx + 4);
-      const bottomRow = lines[nextLineIdx]?.slice(charIdx - 3, charIdx + 4);
+    const topRow = lines[prevLineIdx].slice(charIdx - 3, charIdx + 4);
+    const midRow = lines[lineIdx].slice(charIdx - 3, charIdx + 4);
+    const bottomRow = lines[nextLineIdx]?.slice(charIdx - 3, charIdx + 4);
 
-      const grid = [topRow, midRow, bottomRow];
-      const gridDebug = `
+    const grid = [topRow, midRow, bottomRow];
+    const gridDebug = `
         ${prevLineIdx}: ${topRow}
         ${lineIdx}: ${midRow}
         ${nextLineIdx}: ${bottomRow}
       `;
 
-      // console.log("\n\n", symbol);
-      // console.log(gridDebug);
+    // console.log("\n\n", symbol);
+    // console.log(gridDebug);
 
-      return {
-        id,
-        lineIdx,
-        char,
-        charIdx,
-        validIdx,
-        grid,
-        numbersInGrid: [],
-      } as Symbol;
-    },
-  );
+    return {
+      lineIdx,
+      char,
+      charIdx,
+      grid,
+      gridNumbers: [],
+    } as Symbol;
+  });
 
   symbolsWithGrid.forEach(({ grid }, i) => {
-    let validChars: SurroundingNumber[] = [];
+    let gridNumbers: GridNumber[] = [];
     // console.log("\n\ncheck grid:");
     // console.log(grid.join("\n"));
 
@@ -84,25 +76,25 @@ const parseInput = (rawInput: string) => {
       row.split("").reduce(
         (acc, char, i) => {
           if (char.match(/\d/g)) {
-            acc.chars += char;
+            acc.char += char;
             acc.idxs.push(i);
             if (i === 6) {
               // last digit in grid
-              validChars.push(acc);
+              gridNumbers.push(acc);
             }
             return acc;
           }
 
-          if (acc.chars !== "") {
-            validChars.push(acc);
+          if (acc.char !== "") {
+            gridNumbers.push(acc);
           }
-          return { chars: "", idxs: [] };
+          return { char: "", idxs: [] };
         },
-        { chars: "", idxs: [] } as SurroundingNumber,
+        { char: "", idxs: [] } as GridNumber,
       );
     });
 
-    symbolsWithGrid[i].numbersInGrid = validChars;
+    symbolsWithGrid[i].gridNumbers = gridNumbers;
   });
   return symbolsWithGrid;
 };
@@ -111,11 +103,11 @@ const part1 = (rawInput: string) => {
   const symbols = parseInput(rawInput);
 
   const total = symbols
-    .map((symbol) => symbol.numbersInGrid)
+    .map((symbol) => symbol.gridNumbers)
     .flat()
     .reduce((total, number) => {
       if (number.idxs.some((idx) => isHitIdx(idx))) {
-        total += parseInt(number.chars);
+        total += parseInt(number.char);
       }
       return total;
     }, 0);
@@ -133,7 +125,7 @@ const part2 = (rawInput: string) => {
     .map((symbol) => {
       console.log("\n\ncheck grid:");
       console.log(symbol.grid.join("\n"));
-      const validNumbers = symbol.numbersInGrid.filter((numberInGrid) =>
+      const validNumbers = symbol.gridNumbers.filter((numberInGrid) =>
         numberInGrid.idxs.some((idx) => isHitIdx(idx)),
       );
 
@@ -142,7 +134,7 @@ const part2 = (rawInput: string) => {
         console.log(validNumbers);
 
         symbolTotal =
-          parseInt(validNumbers[0].chars) * parseInt(validNumbers[1].chars);
+          parseInt(validNumbers[0].char) * parseInt(validNumbers[1].char);
         console.log(symbolTotal);
       }
       return symbolTotal;
