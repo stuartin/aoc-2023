b080c33d8a8726dbf06674eee98f6641195c92ef
stuartin <13344713+stuartin@users.noreply.github.com>
2023-12-06 12:55:41 -0800

    day06-pt1

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%86/yellow)](src/day05)
-![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%86/yellow)](src/day06)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
@@ -97,16 +97,16 @@ Both parts: 484.473ms
 
 ```
 Day 05
-Time part 1: 81.01ms
+Time part 1: 12.533ms
 Time part 2: -
-Both parts: 81.01ms
+Both parts: 12.533ms
 ```
 
 ```
 Day 06
-Time part 1: -
+Time part 1: 5.434ms
 Time part 2: -
-Both parts: -
+Both parts: 5.434ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 9/50
-Total time: 1189.077ms
+Total stars: 10/50
+Total time: 1126.034ms
 ```
 
 <!--/RESULTS-->
--- src/day06/index.ts
+++ src/day06/index.ts
@@ -1,11 +1,71 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string) => {
+  // rawInput = `Time:      7  15   30
+  // Distance:  9  40  200`;
+
+  let times: number[] = [];
+  let distances: number[] = [];
+  rawInput.split("\n").forEach((line) => {
+    const lineSplit = line.split(":");
+
+    let lineNumbers = lineSplit[1]
+      .split(" ")
+      .filter((i) => i)
+      .map((i) => parseInt(i));
+
+    if (lineSplit[0].includes("Time")) {
+      times = lineNumbers;
+    }
+
+    if (lineSplit[0].includes("Distance")) {
+      distances = lineNumbers;
+    }
+  });
+
+  return { times, distances };
+};
+
+function getTravelTime(totalTime: number): number[][] {
+  const options = Array.from({ length: totalTime + 1 }).reduce(
+    (output: number[][], _, time) => {
+      if (time === 0 || time === totalTime) {
+        output.push([time, 0]);
+        return output;
+      }
+
+      const timeLeft = totalTime - time;
+
+      output.push([time, timeLeft * time]);
+
+      return output;
+    },
+    [],
+  );
+
+  return options;
+}
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  console.log(input);
+
+  const multiplied = input.times.reduce((total, time, i) => {
+    const travelTimes = getTravelTime(time);
+    // console.log(travelTimes);
+
+    const winners = travelTimes.filter(([time, distance]) => {
+      // console.log({ time, distance, i });
+      // console.log("target", input.distances[i]);
+      return distance > input.distances[i];
+    });
+    console.log(winners.length);
+
+    return total * winners.length;
+  }, 1);
+
+  return multiplied;
 };
 
 const part2 = (rawInput: string) => {
