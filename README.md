27f78039a1c04673ada1522f540275487e926908
stuartin <13344713+stuartin@users.noreply.github.com>
2023-12-09 18:16:45 -0800

    day09-pt1

--- README.md
+++ README.md
@@ -19,7 +19,7 @@
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
-![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%86/yellow)](src/day09)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
@@ -119,15 +119,15 @@ Both parts: 13.8ms
 ```
 Day 08
 Time part 1: -
-Time part 2: 76.55ms
-Both parts: 76.55ms
+Time part 2: 69.417ms
+Both parts: 69.417ms
 ```
 
 ```
 Day 09
-Time part 1: -
+Time part 1: 790.325ms
 Time part 2: -
-Both parts: -
+Both parts: 790.325ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 15/50
-Total time: 41499.59ms
+Total stars: 16/50
+Total time: 42282.782ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day09/index.ts
@@ -0,0 +1,100 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => {
+  // rawInput = `0 3 6 9 12 15
+  //   1 3 6 10 15 21
+  //   10 13 16 21 30 45`;
+
+  const parsed = rawInput.split("\n").map((line) => {
+    const numbers = line.trim().split(" ").map(Number);
+    return numbers;
+  });
+
+  return parsed;
+};
+
+function _getNextLine(line: number[]): number[] {
+  const nextLine = line.flatMap((number, i) => {
+    if (i === 0) return [];
+
+    return number - line[i - 1];
+  });
+
+  return nextLine;
+}
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  console.log(input);
+
+  // 0 = [[line0-0],[line0-1],[line-02]] etc.
+  const lineOfLines = input.map((line) => {
+    // console.log("line", line);
+    let lineResults = [];
+    lineResults.push(line);
+
+    let nextLine: number[] = line;
+    while (!nextLine.every((i) => i === 0)) {
+      nextLine = _getNextLine(nextLine);
+      // console.log("nextLine", nextLine);
+      lineResults.push(nextLine);
+    }
+
+    // console.log("lineResults", lineResults);
+    return lineResults;
+  });
+
+  console.log(lineOfLines);
+
+  const linesNextNumber = lineOfLines.map((subLines) => {
+    let subLineNextNumber = 0;
+    let lastSubLineIdx = subLines.length - 1;
+    while (true) {
+      // console.log({ subLines, lastSubLineIdx });
+      const currLine = subLines[lastSubLineIdx - 1];
+      const prevLine = subLines[lastSubLineIdx];
+      if (!currLine) break;
+
+      subLineNextNumber = currLine[currLine.length - 1] + subLineNextNumber;
+      // console.log("nextNumber", subLineNextNumber);
+      lastSubLineIdx -= 1;
+    }
+
+    console.log("lineNextNumber", subLineNextNumber);
+    return subLineNextNumber;
+  });
+
+  console.log({ linesNextNumber });
+  const total = linesNextNumber.reduce((total, i) => (total += i));
+
+  return total;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
