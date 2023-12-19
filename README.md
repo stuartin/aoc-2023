d30cb39024d000cd82d4d6e8ad894cc878d2acd9
stuartin <13344713+stuartin@users.noreply.github.com>
2023-12-19 11:28:54 -0800

    day15-pt2

--- README.md
+++ README.md
@@ -25,7 +25,7 @@
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
-[![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%86/yellow)](src/day15)
+[![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%85/green)](src/day15)
 ![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/17/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/18/%E2%98%86%E2%98%86/gray)
@@ -167,9 +167,9 @@ Both parts: -
 
 ```
 Day 15
-Time part 1: 1.864ms
-Time part 2: -
-Both parts: 1.864ms
+Time part 1: 1.966ms
+Time part 2: 4.402ms
+Both parts: 6.368ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 20/50
-Total time: 44745.036ms
+Total stars: 21/50
+Total time: 44749.539ms
 ```
 
 <!--/RESULTS-->
--- src/day15/index.ts
+++ src/day15/index.ts
@@ -1,7 +1,7 @@
 import run from "aocrunner";
 
 const parseInput = (rawInput: string) => {
-  rawInput = `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`;
+  // rawInput = `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`;
 
   return rawInput.split(",");
 };
@@ -40,8 +40,8 @@ const part2 = (rawInput: string) => {
     if (step.includes("-")) {
       const label = step.split("-")[0];
       const hashValue = getHashValue(label);
-      console.log({ label, hashValue });
-      console.log(boxes[hashValue]);
+      // console.log({ label, hashValue });
+      // console.log(boxes[hashValue]);
       if (boxes[hashValue] && boxes[hashValue].includes(label)) {
         boxes[hashValue] = boxes[hashValue].filter(
           (boxLabel) => boxLabel !== label,
@@ -51,26 +51,26 @@ const part2 = (rawInput: string) => {
       const label = step.split("=")[0];
       const focalLength = parseInt(step.split("=")[1]);
       const hashValue = getHashValue(label);
-      console.log({ label, focalLength });
-      console.log(boxes[hashValue]);
+      // console.log({ label, focalLength });
+      // console.log(boxes[hashValue]);
 
       if (!boxes[hashValue]) {
-        console.log("new lens");
+        // console.log("new lens");
         boxes[hashValue] = [label];
       }
 
       if (boxes[hashValue] && !boxes[hashValue].includes(label)) {
-        console.log("add lens");
+        // console.log("add lens");
         boxes[hashValue].push(label);
       }
 
       lenses.set(label, focalLength);
-      console.log(lenses);
+      // console.log(lenses);
     }
   });
 
-  console.log(boxes);
-  console.log(lenses);
+  // console.log(boxes);
+  // console.log(lenses);
 
   const total = boxes.reduce((total, box, boxIdx) => {
     if (box && box.length > 0) {
@@ -79,10 +79,10 @@ const part2 = (rawInput: string) => {
       box.forEach((label, slotIdx) => {
         const slotNumber = slotIdx + 1;
         const focalLength = lenses.get(label);
-        console.log({ slotNumber, focalLength });
+        // console.log({ slotNumber, focalLength });
 
         const power = boxNumber * slotNumber * focalLength;
-        console.log({ power });
+        // console.log({ power });
         total += power;
       });
     }
