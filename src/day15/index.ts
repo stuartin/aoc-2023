87a7c2ebb5a986781be1caefcb8cecbb759ea7de
stuartin <13344713+stuartin@users.noreply.github.com>
2023-12-19 11:27:55 -0800

    working sample data...

--- src/day15/index.ts
+++ src/day15/index.ts
@@ -33,7 +33,64 @@ const part1 = (rawInput: string) => {
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  const boxes: string[][] = Array.from({ length: 256 });
+  const lenses = new Map();
+
+  input.forEach((step) => {
+    if (step.includes("-")) {
+      const label = step.split("-")[0];
+      const hashValue = getHashValue(label);
+      console.log({ label, hashValue });
+      console.log(boxes[hashValue]);
+      if (boxes[hashValue] && boxes[hashValue].includes(label)) {
+        boxes[hashValue] = boxes[hashValue].filter(
+          (boxLabel) => boxLabel !== label,
+        );
+      }
+    } else if (step.includes("=")) {
+      const label = step.split("=")[0];
+      const focalLength = parseInt(step.split("=")[1]);
+      const hashValue = getHashValue(label);
+      console.log({ label, focalLength });
+      console.log(boxes[hashValue]);
+
+      if (!boxes[hashValue]) {
+        console.log("new lens");
+        boxes[hashValue] = [label];
+      }
+
+      if (boxes[hashValue] && !boxes[hashValue].includes(label)) {
+        console.log("add lens");
+        boxes[hashValue].push(label);
+      }
+
+      lenses.set(label, focalLength);
+      console.log(lenses);
+    }
+  });
+
+  console.log(boxes);
+  console.log(lenses);
+
+  const total = boxes.reduce((total, box, boxIdx) => {
+    if (box && box.length > 0) {
+      const boxNumber = boxIdx + 1;
+
+      box.forEach((label, slotIdx) => {
+        const slotNumber = slotIdx + 1;
+        const focalLength = lenses.get(label);
+        console.log({ slotNumber, focalLength });
+
+        const power = boxNumber * slotNumber * focalLength;
+        console.log({ power });
+        total += power;
+      });
+    }
+
+    return total;
+  }, 0);
+
+  return total;
 };
 
 run({
